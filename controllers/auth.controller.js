2a7a82df2dfe0e96b36998413dac51b93a4c3992
Catuxa.aljo <catuxa.aljo@gmail.com>
2021-06-25 06:59:56 +0200

    feat: login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -32,3 +32,39 @@ module.exports.doRegister = (req, res, next) => {
         }
       })
   }
+
+  module.exports.login = (req, res, next) => {
+    res.render('auth/login')
+
+  }
+
+  module.exports.doLogin = (req, res, next) => {
+
+    function renderLoginWithErrors(){
+      res.render('auth/login', {
+        user: req.body,
+        errors: 'Invalid password or email'
+      })
+    }
+
+    User.findOne({email: req.body.email})
+      .then(user => {
+        if(!user){
+          renderLoginWithErrors()
+        } else {
+          return user.checkPassword(req.body.password)
+                  .then(match => {
+                    if(!match) {
+                      renderLoginWithErrors()
+                    } else {
+                      res.send('/')
+                    }
+                  }
+                   
+                  )
+        }
+      }
+        
+      )
+      .catch()
+  }
