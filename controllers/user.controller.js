eea758224d015fb76a087ae95e73f8483f13b7c0
Edgar HiSo <edghiso@gmail.com>
2021-06-29 20:08:20 +0200

    fix: identation

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -8,36 +8,26 @@ module.exports.userProfile = (req, res, next) => {
 
 module.exports.userProfileEdit = (req, res, next) => {
 
-    User
-    .findOne({_id: req.params.id})
+    User.findOne({_id: req.params.id})
     .then(user => {
         res.render('user/edit', {
             user,
             categories
         })
     })
-    .catch(error => {
-        if (error instanceof mongoose.Error.ValidationError) {
-          renderWithErrors(error.errors)
-        } else {
-          next(error)
-        }
-      })
+    .catch(error => next(error))
 }
 
 module.exports.userProfileDoEdit = (req, res, next) => {
     
-     User
-    .findByIdAndUpdate(req.params.id, {
-        name: req.body.name,
-        avatar: req.file.path,
-        age: req.body.age,
-        interests: req.body.interests,
-        city: req.body.city,
-    }, { runValidators: true, new: true })   
-    .then(user => {        
-        res.redirect(`/user-profile/${user._id}`)
-    })     
-    .catch(error => next(error))
+     User.findByIdAndUpdate(req.params.id, {
+            name: req.body.name,
+            avatar: req.file.path,
+            age: req.body.age,
+            interests: req.body.interests,
+            city: req.body.city,
+        }, { runValidators: true, new: true })   
+        .then(user => res.redirect(`/user-profile/${user.id}`))     
+        .catch(error => next(error))
 
 }
