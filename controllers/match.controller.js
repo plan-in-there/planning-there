3131b8b1c862d18b9e230b33e49847fc352f1f85
Edgar HiSo <edghiso@gmail.com>
2021-07-03 14:04:58 +0200

    feat: delete match

--- controllers/match.controller.js
+++ controllers/match.controller.js
@@ -2,15 +2,31 @@ const mongoose = require('mongoose')
 const Match = require('../models/match.model')
 
 module.exports.like = (req, res, next) => {
-    console.log(req.user.id)
-    console.log(req.params.id)
+    Match.findOne({eventId : req.params.id, userId: req.user.id})
+        .then(match => {
+            if(!match) {
+                const match = new Match({
+                    userId : req.user.id,
+                    eventId : req.params.id
+                })
+                match.save()
+                    .then(matches => res.redirect('/events'))
 
-    const match = new Match({
+            } else {
+              return  Match.findByIdAndDelete(match.id)
+                .then(() => res.redirect('/'))
+            }
+        })
+        .catch(next)
+
+
+
+   /*  const match = new Match({
         userId : req.user.id,
         eventId : req.params.id
     })
     match.save()
         .then(matches => res.redirect('/events'))
-        .catch()
+        .catch() */
 }
 
