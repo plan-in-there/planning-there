2377b384dd8345ef5d1d6ea18d71e721b0593d89
Edgar HiSo <edghiso@gmail.com>
2021-07-09 16:42:07 +0200

    feat:messages

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -1,27 +1,9 @@
-// doCreate 
-// req.paramas.id =  el del usuario con el que quiero hablar
-// req.user._id
-// Chat.save()
-// .populate(users)
-//.then(chat =>
-// vista que será tipo users/profile/chats/${chat.id}
-//)
-
-// const Chat = require("../models/message.model");
-
-
-// doChat => {
-    // Chat.findById(REQ.PARAMS.ID)
-    // .populate(meessages)
-    // .popiulate(users)
-// renderiza la vista del chat
-//}
-
 const mongoose = require('mongoose')
 const Chat = require('../models/chat.model')
 const Event = require('../models/event.model')
 const User = require('../models/user.model')
 const Match = require('../models/match.model')
+const Message = require('../models/message.model')
 
 module.exports.getChat = (req, res, next) => {
     Chat.findById(req.params.id)
@@ -33,10 +15,6 @@ module.exports.getChat = (req, res, next) => {
         .catch(next)
 }
 
-module.exports.showChat = (req, res, next) => {
-  const chat = req.chat 
-    render("/html", {chat})
-}
 
 //Chat.find({ users.includes(req.param.id) &&  req.user._id})
 // .then(chat => {
@@ -45,7 +23,7 @@ module.exports.showChat = (req, res, next) => {
 //         Chat.create(chat)
 //             .then(res.render('chat/form'))
 //     } else {
-module.exports.doCreate = (req, res, next) => {
+/* module.exports.doCreate = (req, res, next) => {
     const chat = {
         users: [req.params.id, req.user._id],
         messages: []
@@ -55,13 +33,47 @@ module.exports.doCreate = (req, res, next) => {
             res.redirect(`/user-profile/chat/${chat._id}`)
         })
         .catch(next)
+} */
+
+module.exports.doCreate = (req, res, next) => {
+    const chat = {
+        users: [req.params.id, req.user._id],
+        messages: []
+    }
+    Chat.findById(req.params.id)
+        .then(chat => {
+            if (chat) {
+                res.redirect(`/user-profile/chat/${chat._id}`)
+            } else {
+               return Chat.create(chat)
+                    .then(chat => {
+                        res.redirect(`/user-profile/chat/${chat._id}`)
+                    })
+            }
+        })
+        .catch(next)
 }
 
 
+
 module.exports.newMessage = (req, res, next) => {
-    // Un form en el front end que al submit guarda
-    // un mensaje en el chat
-    const chat = req.chat
-    const message = req.body.message
-    // save()
+   
+    console.log("new message")
+    Chat.findById(req.params.id)
+        .then(chat => {
+            console.log("found chat")
+            const message = req.body.message
+           return  Message.create({message})
+            .then(message => {
+                console.log("new message inside chat")
+             return Chat.updateOne(
+                    { _id: chat._id},
+                    { $push: { messages: message}} 
+                    )
+                    .then(() => {
+                        res.redirect(`/user-profile/chat/${chat._id}`)
+                    })
+            })
+        })
+        .catch(next)
 }
