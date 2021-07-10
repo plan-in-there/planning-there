43ffd5af324f20343946c7d077af9aae3371e530
Edgar HiSo <edghiso@gmail.com>
2021-07-10 09:00:55 +0200

    ..

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -16,25 +16,6 @@ module.exports.getChat = (req, res, next) => {
 }
 
 
-//Chat.find({ users.includes(req.param.id) &&  req.user._id})
-// .then(chat => {
-//     if (!chat) {
-//         chat = req.body
-//         Chat.create(chat)
-//             .then(res.render('chat/form'))
-//     } else {
-/* module.exports.doCreate = (req, res, next) => {
-    const chat = {
-        users: [req.params.id, req.user._id],
-        messages: []
-    }
-    Chat.create(chat)
-        .then(chat => {
-            res.redirect(`/user-profile/chat/${chat._id}`)
-        })
-        .catch(next)
-} */
-
 module.exports.doCreate = (req, res, next) => {
     const chat = {
         users: [req.params.id, req.user._id],
@@ -57,15 +38,11 @@ module.exports.doCreate = (req, res, next) => {
 
 
 module.exports.newMessage = (req, res, next) => {
-   
-    console.log("new message")
     Chat.findById(req.params.id)
         .then(chat => {
-            console.log("found chat")
             const message = req.body.message
            return  Message.create({message})
             .then(message => {
-                console.log("new message inside chat")
              return Chat.updateOne(
                     { _id: chat._id},
                     { $push: { messages: message}} 
